a6a5a096d536f79663500ef552d5fd0645f6c997
Abdul Mumit <mamumit07@gmail.com>
2018-11-28 21:45:13 +0100

    Add framework in template

--- playground/template.cpp
+++ playground/template.cpp
@@ -36,9 +36,30 @@ void send(
   Send(to);
 }
 
+// head -> ... -> tail
+// head: who prints final result, don't send to anyone
+// tail: who doesn't receive from anyone
+
+#define is_head(id) ((id) == 0)
+#define is_tail(id) ((id) == nodes-1)
+#define send_to(id) ((id) - 1)
+#define receive_from(id) ((id) + 1)
+
 int main() {
   int id = MyNodeId();
   int nodes = NumberOfNodes();
   // long long N = GetN();
+  if (!is_tail(id)) {
+    receive(
+      receive_from(id)
+    );
+  }
+  if (!is_head(id)) {
+    send(
+      send_to(id)
+    );
+  } else {
+    // cout << ans << endl;
+  }
   return 0;
 }
